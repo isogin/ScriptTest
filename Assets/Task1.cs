4dedc753b38fae5eb1bf215c6364269312560391
isogin <daxiangy147@gmail.com>
2021-02-09 10:45:29 +0900

    Initial commit

---
+++ Assets/Task1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = new int[5];  //好きな値で初期化
+        int x, i;
+
+        //配列の各要素を順番に表示
+        for (i = 0; i < 5; i++)
+        {
+            x = Random.Range(1, 10);
+            array[i] = x;
+            Debug.Log(array[i]);
+        }
+
+
+        //配列の各要素を逆順に表示
+        for(i = 0; i < 5; i++)
+        {
+            Debug.Log(array[4 - i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TaskDeveloped.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    int hp;
+    int power;
+    int mp = 53;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+
+        this.hp -= damage;
+    }
+
+    public void Magic()
+    {
+        if (mp > 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+
+public class TaskDeveloped : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boss laseboss = new Boss();
+
+        laseboss.Attack();
+
+        laseboss.Defence(3);
+
+        laseboss.Magic();
+
+        for(int i = 0; i < 10; i++)
+        {
+            laseboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
